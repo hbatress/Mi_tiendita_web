b856b9eec3a04f408ce175eb7eb8c18fc27b6e91
Herber Batres <hbatress@miumg.edu.gt>
2024-12-14 20:45:33 -0600

    3 procedimintos almacenados creados

--- back-end/routes/routes.js
+++ back-end/routes/routes.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const routes=express.Router();
 const {sql,poolPromise}=require('../db/database');
+routes.use(express.json()); 
 
 routes.get('/',(req,res)=>{
     res.send('Saludos desde Express');
@@ -17,4 +18,38 @@ routes.get('/productos',async(req,res)=>{
     }
 
 });
+
+routes.get('/clientes',async(req,res)=>{
+    const estado = req.query.estado; // Suponiendo que el ID del cliente se envía como parámetro de consulta
+    try {
+        const pool = await poolPromise;
+        const result = await pool.request()
+            .input('estado', sql.Int, estado) // Enviar el parámetro al procedimiento almacenado
+            .execute('Listar_clientes_pendientes'); // Nombre del procedimiento almacenado
+        res.json(result.recordset);
+    } catch (error) {
+        res.status(500).send('Error en la consulta');
+    }
+
+});
+
+routes.post('/procesar_orden', async (req, res) => {
+    const { id_usuario, id_direccion, id_estado, orden_detalles, id_metodo_pago, nombre_factura, nit } = req.body; // Suponiendo que los datos se envían en el cuerpo de la solicitud
+    try {
+        const pool = await poolPromise;
+        const result = await pool.request()
+            .input('id_usuario', sql.Int, id_usuario)
+            .input('id_direccion', sql.Int, id_direccion)
+            .input('id_estado', sql.Int, id_estado)
+            .input('orden_detalles', sql.NVarChar(sql.MAX), orden_detalles)
+            .input('id_metodo_pago', sql.Int, id_metodo_pago)
+            .input('nombre_factura', sql.VarChar(200), nombre_factura)
+            .input('nit', sql.VarChar(20), nit)
+            .execute('procesar_orden'); 
+        res.json(result.recordset);
+    } catch (error) {
+        res.status(500).send('Error en la consulta');
+    }
+});
+
 module.exports=routes;
--- back-end/server/server.js
+++ back-end/server/server.js
@@ -1,19 +1,11 @@
-const https=require('https');
-const fs=require('fs');
-const path=require('path');
 const express=require('express');
 const routes=require('../routes/routes');
 
 
 const app=express();
-const options={
-    key:fs.readFileSync(path.join(__dirname,'../key.pem')),
-    cert:fs.readFileSync(path.join(__dirname,'../cert.pem'))
-}
-
-const server=https.createServer(options,app);
+app.use(express.json());
 app.use('/',routes);
 const port=3000;
-server.listen(port,()=>{
+app.listen(port,()=>{
     console.log(`Server is running on https://localhost:${port}`);
 });
